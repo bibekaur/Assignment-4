fb224ea64917a8ddd310aec787f68c77013b03bd
bibekaur <bibekaur@gmail.com>
2016-11-02 22:35:55 -0700

    Few changes to allow multiple password changes

--- task4.py
+++ task4.py
@@ -2,7 +2,7 @@ import base64
 import hashlib
 
 SIZE_OF_SHA1_HASH = 40
-OLD_HASH = "d6515dc7e1dcd69e6fce6dd9ea2dc0fa52fe20c0"
+OLD_HASH_INDEX = 245854
 
 userPassword = raw_input("Please enter the desired password for 15093123.program2.exe : ")
 hashObj = hashlib.sha1()
@@ -14,10 +14,9 @@ with open("15093123.program2.exe", mode='rb') as file:
     fileContent = file.read()
     
 s = fileContent.encode("hex")
-hashIndex = s.find(OLD_HASH)
 
 l = list(s)
-l[hashIndex:hashIndex+SIZE_OF_SHA1_HASH] = newHash
+l[OLD_HASH_INDEX:OLD_HASH_INDEX+SIZE_OF_SHA1_HASH] = newHash
 s = "".join(l)
 
 d = s.decode("hex")
